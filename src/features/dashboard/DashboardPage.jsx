e59f85f86bcb83c66d72a21da761ac898a819bdf
Yogendra-SinghRathore <yrathore103@gmail.com>
2026-03-04 20:41:05 +0530

    dashboard count

--- src/features/dashboard/DashboardPage.jsx
+++ src/features/dashboard/DashboardPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useUsers } from '../users/context/UsersContext'
 
 const DashboardPage = () => {
+
+  const { users } = useUsers();
+
   return (
     <div>
       <h2>Dashboard</h2>
+      <div className="col-2">
+        <div className="card text-bg-light mb-3">
+          <div className="card-header">Total Users</div>
+          <div className="card-body">
+            <h5 className="card-title">{users.length}</h5>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
